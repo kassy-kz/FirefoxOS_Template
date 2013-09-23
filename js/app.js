3094658af88298efb7b07b2226a75fdf18348750
kassy-kz <kashimoto.kazutoshi@gmail.com>
2013-09-23 14:18:09 +0900

    振動機能をひと通り実装

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 // onCreateに相当
 window.onload = function() {
-	// findViewByIdに相当
-	var btn1 = document.getElementById('btn1');
-	// setOnClickListnerに相当
-	btn1.onclick = function() {
-	    document.getElementById('div1').innerHTML = 'Hello World';
+
+	onButtonClick1 = function() {
+	 	window.navigator.vibrate(200);
+	}
+
+	onButtonClick2 = function() {
+	 	window.navigator.vibrate([400,50,100,50,100]);		
+	}
+
+	onButtonClick3 = function() {
+	 	window.navigator.vibrate(5000);		
+	}
+
+	onButtonClick4 = function() {
+	 	window.navigator.vibrate(0);		
 	}
 }
