f412ed7bf9d6bfb900d3e5cebb88bf36598a499e
kassy-kz <kashimoto.kazutoshi@gmail.com>
2013-09-24 21:43:44 +0900

    使用メソッド変更

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
-window.onload = function() {
-	battery.addEventListener("chargingchange", updateBatteryStatus);
-	battery.addEventListener("levelchange", updateBatteryStatus);
-	updateBatteryStatus();
-}
-
 var battery = navigator.battery ;
 
-function updateBatteryStatus() {
+battery.onlevelchange = function() {
 	var status = "Battery status: " + battery.level * 100 + " %";
   	document.getElementById('div1').innerHTML = status;    
+}
 
+battery.onchargingchange = function () {
     if (battery.charging) {
     	document.getElementById('div2').innerHTML = "Battery is charging";    
     } else {
     	document.getElementById('div2').innerHTML = "Battery is not charging";        	
     }
 }
+
