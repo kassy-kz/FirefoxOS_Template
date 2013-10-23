bbdd402de026f7157e72db0dafcaaa46fdee70a3
kassy-kz <kashimoto.kazutoshi@gmail.com>
2013-10-23 21:44:38 +0900

    5000 timer

--- js/app.js
+++ js/app.js
@@ -1,47 +1,34 @@
 
 var alarmId;
 
-// onCreateに相当
-window.onload = function() {
-	// findViewByIdに相当
-	var btn1 = document.getElementById('btn1');
-
-}
-
-// setOnClickListnerに相当
+// ボタンが押された時、Alarmをセットする
 var onButtonClick1 = function() {
-	// Set an alarm and store it's id
-	//var request = navigator.mozAlarms.add(new Date("Sep 29, 2013 18:29:00"), "honorTimezone");
-	var request = navigator.mozAlarms.add(new Date(2013,8,29,19,25,0,0), "honorTimezone");
-	 var time = 4;
-	  // var request = navigator.mozAlarms.add(
-	  //   new Date((new Date()).getTime() + time * 1000),
-	  //   "honorTimezone",
-	  //   { mydata: "alarm" }
-	  // );
+    // Alarm発火までの時間
+	var time = 5000;
+	// Alarmセット
+	var request = navigator.mozAlarms.add(
+	    new Date((new Date()).getTime() + time),
+	    "honorTimezone",
+	    { message: "alarm1" }
+	);
 
+	// Alarmセットに成功した時
 	request.onsuccess = function () {
-	  alarmId = this.result;
+	    alarmId = this.result;
 	}
 }
 
-// setOnClickListnerに相当
+// ボタンが押された時、Alarmを解除する
 var onButtonClick2 = function() {
 	if (alarmId) {
-	  navigator.mozAlarms.remove(alarmId);
+	    navigator.mozAlarms.remove(alarmId);
 	}
 }
 
-navigator.mozSetMessageHandler("alarm", function (mozAlarm) { 
-//   alert("alarm fired: " + JSON.stringify(mozAlarm.data)); 
-    // 通知（ノーティフィケーション）生成
-  var notification = navigator.mozNotification.createNotification(
-    "10秒たちました",
-    "右スワイプでクリアします。"
-  );
-  notification.show();
+// Alarmのハンドリング
+navigator.mozSetMessageHandler("alarm", alarmHandler);
 
-});
-
-// 
-
+// Alarmが発火したときに呼ばれる関数
+var alarmHandler = function(mozAlarm) {
+    alert("alarm fired: " + JSON.stringify(mozAlarm.data)); 	
+}
