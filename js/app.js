bdd77e6fcd94c767147af83e52a01ff6ba6c6809
kassy-kz <kashimoto.kazutoshi@gmail.com>
2013-11-06 06:52:15 +0900

    Activity呼び出し成功

--- js/app.js
+++ js/app.js
@@ -1,10 +1,45 @@
 // onCreateに相当
 window.onload = function() {
-	// findViewByIdに相当
-	var btn1 = document.getElementById('btn1');
 }
 
 // setOnClickListnerに相当
-var onButtonClick = function() {
-    document.getElementById('div1').innerHTML = 'Hello World';
+var onButtonClick1 = function() {
+	var activity = new MozActivity({
+	    // Ask for the "pick" activity
+	    name: "pick",
+
+	    // Provide the data required by the filters of the activity
+	    data: {
+	        type: "image/jpeg"
+	    }
+	});
+
+	activity.onsuccess = function() {
+	    var picture = this.result;
+	    console.log("A picture has been retrieved");
+
+	    // 画像を生成し、返ってきた Blob を src にセット
+	    var img = document.createElement("img");
+	    img.src = window.URL.createObjectURL(this.result.blob);
+		img.width = 160;
+	    // アプリ内に画像を表示
+	    document.getElementById("div2").appendChild(img);
+
+	};
+
+	activity.onerror = function() {
+	    console.log(this.error);
+	};
+}
+
+var onButtonClick2 = function() {
+	var activity = new MozActivity({
+	    // Ask for the "pick" activity
+	    name: "configure",
+
+	    // Provide the data required by the filters of the activity
+	    data: {
+	        target : "device"
+	    }
+	});
 }
